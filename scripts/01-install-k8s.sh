9398a4c77d808de32af5846026bc4d1e73bff9d4
Dmitry <dimich14@gmail.com>
2026-08-17 17:33:41 +0300

    skip interactive install kuber claster

--- scripts/01-install-k8s.sh
+++ scripts/01-install-k8s.sh
@@ -10,6 +10,9 @@
 
 set -e
 
+# Отключаем интерактивные запросы для всех apt-команд
+export DEBIAN_FRONTEND=noninteractive
+
 NODE_TYPE=$1
 
 if [ -z "$NODE_TYPE" ]; then
@@ -37,21 +40,21 @@ sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Устанавливаем зависимости
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+sudo -E apt-get update
+sudo -E apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
 
 # Устанавливаем Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo -E apt-get update
+sudo -E apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
 # Устанавливаем kubeadm, kubelet, kubectl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.36/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://mirror.yandex.ru/mirrors/pkgs.k8s.io/core/stable/v1.36/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo -E apt-get update
+sudo -E apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # Включаем модули ядра для Kubernetes
