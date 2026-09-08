5281b1b48efe7b879986db1a63d8069e43287fe0
Dmitry <dimich14@gmail.com>
2026-09-08 17:12:15 +0300

    add terraform & tofu worker nodes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,59 @@
-# terraform/main.tf
-
+# main.tf
 terraform {
   required_providers {
     multipass = {
-      source = "todoroff/multipass"
+      source  = "todoroff/multipass"
       version = "~> 1.7"
     }
   }
 }
 
 provider "multipass" {
-  # Базовые настройки (пока оставляем пустым)
+  # Можно оставить пустым
 }
 
+# Мастер-нода
 resource "multipass_instance" "master" {
   name   = "k8s-master"
-  image  = "lts"    # Ubuntu 22.04 LTS
+  image  = "lts"
   cpus   = 2
   memory = "2G"
   disk   = "10G"
-  
-  # Опционально: можно добавить cloud-init для первоначальной настройки
-  # cloud_init = file("cloud-init.yaml")
+
+  # Блок для выполнения скриптов после создания
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from master node'"
+    ]
+  }
+}
+
+# Воркер-нода 1
+resource "multipass_instance" "worker1" {
+  name   = "k8s-worker1"
+  image  = "lts"
+  cpus   = 2
+  memory = "2G"
+  disk   = "10G"
+}
+
+# Воркер-нода 2
+resource "multipass_instance" "worker2" {
+  name   = "k8s-worker2"
+  image  = "lts"
+  cpus   = 2
+  memory = "2G"
+  disk   = "10G"
+}
+
+# Вывод IP-адресов
+output "master_ip" {
+  value = multipass_instance.master.ipv4
+}
+
+output "worker_ips" {
+  value = [
+    multipass_instance.worker1.ipv4,
+    multipass_instance.worker2.ipv4
+  ]
 }
